ffa954bea1807971244d757dc0ed2f78f5c23bec
Max Desiatov <max@desiatov.com>
2018-10-05 21:29:50 +0100

    Initial commit

---
+++ highlightCode.js
@@ -0,0 +1,42 @@
+const CodeMirror = require("codemirror/addon/runmode/runmode.node");
+
+require("codemirror/mode/clike/clike");
+require("codemirror/mode/javascript/javascript");
+require("codemirror/mode/swift/swift");
+require("codemirror/mode/sql/sql");
+require("codemirror/mode/shell/shell");
+require("codemirror/mode/ruby/ruby");
+require("codemirror/mode/python/python");
+require("codemirror/mode/markdown/markdown");
+require("codemirror/mode/erlang/erlang");
+require("codemirror/mode/haskell/haskell");
+require("codemirror/mode/elm/elm");
+require("codemirror/mode/jsx/jsx");
+require("codemirror/mode/go/go");
+require("codemirror/mode/lua/lua");
+
+module.exports = function highlightCode(language, value) {
+  const elements = [];
+  let lastStyle = null;
+  let tokenBuf = "";
+  const pushElement = (token, style) => {
+    elements.push(
+      `<span${style ? ` class="cm-${style}"` : ""}>${token}</span>`
+    );
+  };
+  CodeMirror.runMode(value, language, (token, style) => {
+    if (lastStyle === style) {
+      tokenBuf += token;
+      lastStyle = style;
+    } else {
+      if (tokenBuf) {
+        pushElement(tokenBuf, lastStyle);
+      }
+      tokenBuf = token;
+      lastStyle = style;
+    }
+  });
+  pushElement(tokenBuf, lastStyle);
+
+  return elements.join("");
+};
---
+++ index.js
@@ -0,0 +1,18 @@
+module.exports = ({ markdownAST }, { theme, className } = {}) => {
+  visit(markdownAST, `code`, node => {
+    let language = node.lang;
+
+    // Replace the node with the markup we need to make
+    // 100% width highlighted code lines work
+    node.type = `html`;
+
+    node.value = `
+    <div class="gatsby-highlight" data-language="${languageName}">
+      <pre class="${className} cm-s-${theme}">
+        <code>
+          ${highlightCode(language, node.value)}
+        </code>
+      </pre>
+    </div>`;
+  });
+};
